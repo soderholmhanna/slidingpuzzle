0c5e4aa3a4b19e11e077eed530b78462ca1999b9
Hanna <hms.soderholm@gmail.com>
2024-03-23 15:15:53 +0100

    added puzzle board and css for puzzle

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+    <h1>Sliding puzzle</h1>
+    <div id="puzzle">
+      <div className="piece">1</div>
+      <div className="piece">2</div>
+      <div className="piece">3</div>
+      <div className="piece">4</div>
+      <div className="piece">5</div>
+      <div className="piece">6</div>
+      <div className="piece">7</div>
+      <div className="piece">8</div>
+      <div className="piece">9</div>
+      <div className="piece">10</div>
+      <div className="piece">11</div>
+      <div className="piece">12</div>
+      <div className="piece">13</div>
+      <div className="piece">14</div>
+      <div className="piece">15</div>
+    </div>
+   </>
   )
 }
 
-export default App
+export default App;
