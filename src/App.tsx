ed61c9f2ff90839de90bdca924be1d8fe7a77fce
Hanna <hms.soderholm@gmail.com>
2024-05-10 18:36:08 +0200

    moved css to separate assets folder, tidied up code in app.tsx, changed title of page in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
 import { useState } from "react";
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+const shuffle = (array: number[]) => {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = newArray[i];
+      newArray[i] = newArray[j]
+      newArray[j] = temp;
+  }
+  return newArray;
+};
 
 
 function App() {
-  const [pieces, setPieces] = useState(numbers);
 
-  const shuffle = (array: number[]) => {
-    const newArray = [...array];
-    for (let i = newArray.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = newArray[i];
-        newArray[i] = newArray[j]
-        newArray[j] = temp;
-    }
-    return newArray;
-  };
-
+  const [pieces, setPieces] = useState(shuffle(numbers));  
 
   const handleClick = () => {
     setPieces(shuffle(pieces))
   }
 
   return (
-   <>
+   <div className="puzzle-container">
     <h1>Sliding puzzle</h1>
     <div id="puzzle">
       {
-        pieces.map((piece, index) => {
+        pieces.map((piece) => {
           return <div key={piece} className="piece">{piece}</div>
         })
       }
     </div>
     <button id="shuffle" onClick={handleClick}>Shuffle pieces</button>
-   </>
+   </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import './puzzle.css'
+import './assets/index.css'
+import './assets/puzzle.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
