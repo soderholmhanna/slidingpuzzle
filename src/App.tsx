1d3a754a5b76ff4c504b80a7e2544df03567b0e8
Hanna <hms.soderholm@gmail.com>
2024-05-12 12:46:00 +0200

    moved remaining code to Game component, changed board to 3x3, added functions to calc index of pieces

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import { useState } from "react";
 import Game from "./components/Game";
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
-const shuffle = (array: number[]) => {
-  const newArray = [...array];
-  for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = newArray[i];
-      newArray[i] = newArray[j]
-      newArray[j] = temp;
-  }
-  return newArray;
-};
-
-
 function App() {
-  const [pieces, setPieces] = useState(shuffle(numbers));  
-
-  const handleClick = () => {
-    setPieces(shuffle(pieces))
-  }
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
-    <Game
-      onShuffle={handleClick}
-      pieces={pieces}
-    />
+    <Game blocks={numbers}/>
   )
 }
 
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const shuffle = (array: number[]) => {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = newArray[i];
+      newArray[i] = newArray[j]
+      newArray[j] = temp;
+  }
+  return newArray;
+};
 
 interface Props {
-    onShuffle: () => void;
-    pieces: number[];
+  blocks: number[];
 }
 
-const Game: React.FC<Props> = ({ onShuffle, pieces }) => {
+const Game: React.FC<Props> = ({ blocks }) => {
+  const emptyBlockValue = 16;
+
+  const [pieces, setPieces] = useState<number[]>(shuffle(blocks));
+
+  const canMove = (pieceIndex: number): boolean => {
+    const pieceRow = Math.floor(pieceIndex / 4);
+    const pieceCol = pieceIndex - pieceRow * 4;
+
+    const emptyPieceIndex = pieces.indexOf(emptyBlockValue);
+    const emptyPieceRow = Math.floor(emptyPieceIndex / 4);
+    const emptyPieceCol = emptyPieceIndex % 4;
+
+    return (
+      pieceRow === emptyPieceRow && Math.abs(pieceCol - emptyPieceCol) === 1 ||
+      pieceCol === emptyPieceCol && Math.abs(pieceRow - emptyPieceRow) === 1
+    )
+  }
+
+  const movePiece = (pieceIndex: number) => {
+    if (canMove(pieceIndex) === true) {
+      console.log("Clicked piece with index:", pieceIndex)
+      const emptyIndex = pieces.indexOf(emptyBlockValue);
+      const newPieces = [...pieces];
+      newPieces[emptyIndex] = pieces[pieceIndex];
+      newPieces[pieceIndex] = emptyBlockValue;
+      setPieces(newPieces);
+    } else {
+      console.log("Can't move piece:", pieceIndex)
+    }
+  };
+  
+  
   return (
     <div className="puzzle-container">
     <h1>Sliding Puzzle</h1>
     <div id="puzzle">
       {
-        pieces.map((piece) => {
-          return <div key={piece} className="piece">{piece}</div>
+        pieces.map((piece, index) => {
+          return <div key={piece} className="piece" onClick={() => movePiece(index)}>{piece}</div>
         })
       }
     </div>
-    <button id="shuffle" onClick={onShuffle}>Shuffle pieces</button>
+    <button id="shuffle">Shuffle pieces</button>
    </div>
   )
 }
 
-export default Game;
+export default Game; 
