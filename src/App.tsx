8652c900e22a18ec4b3c29aa76c8e7b13d2bfd33
Hanna <hms.soderholm@gmail.com>
2024-05-12 19:14:31 +0200

    added styling to empty block, lots of console logs to find bugs, fixed calc functions, moving pieces now works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Game from "./components/Game";
 
 function App() {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <Game blocks={numbers}/>
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -16,49 +16,80 @@ interface Props {
 }
 
 const Game: React.FC<Props> = ({ blocks }) => {
-  const emptyBlockValue = 16;
+  const [emptyBlockValue, setEmptyBlockValue] = useState<number>(9);
 
   const [pieces, setPieces] = useState<number[]>(shuffle(blocks));
 
   const canMove = (pieceIndex: number): boolean => {
-    const pieceRow = Math.floor(pieceIndex / 4);
-    const pieceCol = pieceIndex - pieceRow * 4;
+    const pieceRow = Math.floor(pieceIndex / 3);
+    const pieceCol = pieceIndex - pieceRow * 3;
 
     const emptyPieceIndex = pieces.indexOf(emptyBlockValue);
-    const emptyPieceRow = Math.floor(emptyPieceIndex / 4);
-    const emptyPieceCol = emptyPieceIndex % 4;
+    const emptyPieceRow = Math.floor(emptyPieceIndex / 3);
+    const emptyPieceCol = emptyPieceIndex % 3;
 
-    return (
+    if (
       pieceRow === emptyPieceRow && Math.abs(pieceCol - emptyPieceCol) === 1 ||
       pieceCol === emptyPieceCol && Math.abs(pieceRow - emptyPieceRow) === 1
-    )
+    ) {
+      return true;
+    } else {
+      console.log("Piece is in column:", pieceCol)
+      console.log("Piece is in row:", pieceRow)
+      console.log("Empty piece has index:", emptyPieceIndex)
+      console.log("Empty block value:", emptyBlockValue)
+      console.log("Empty piece is in row:", emptyPieceRow)
+      console.log("Empty piece is in column:", emptyPieceCol)
+      return false;
+    }
+      
   }
 
   const movePiece = (pieceIndex: number) => {
     if (canMove(pieceIndex) === true) {
       console.log("Clicked piece with index:", pieceIndex)
+
       const emptyIndex = pieces.indexOf(emptyBlockValue);
+      console.log("Empty block index is:", emptyIndex)
+
+
       const newPieces = [...pieces];
       newPieces[emptyIndex] = pieces[pieceIndex];
       newPieces[pieceIndex] = emptyBlockValue;
       setPieces(newPieces);
     } else {
       console.log("Can't move piece:", pieceIndex)
+      console.log(pieces)
     }
   };
   
+  const handleShuffle = () => {
+    setPieces(shuffle(pieces))
+  }
+
+  const handleMovePiece = (pieceIndex: number) => {
+    movePiece(pieceIndex)
+  }
   
   return (
     <div className="puzzle-container">
-    <h1>Sliding Puzzle</h1>
-    <div id="puzzle">
-      {
-        pieces.map((piece, index) => {
-          return <div key={piece} className="piece" onClick={() => movePiece(index)}>{piece}</div>
-        })
-      }
+      <h1>Sliding Puzzle</h1>
+      <div id="puzzle">
+      {pieces.map((piece, index) => {
+        if (piece !== 9) {
+          return (
+            <div key={piece} className=" piece" onClick={() => handleMovePiece(index)}>
+              {piece}
+            </div>
+          );
+        } else if (piece === 9) {
+          return (
+            <div key={piece} className="piece" id="empty-piece"></div>
+          );
+        }
+      })}
     </div>
-    <button id="shuffle">Shuffle pieces</button>
+    <button id="shuffle" onClick={handleShuffle}>Shuffle pieces</button>
    </div>
   )
 }
